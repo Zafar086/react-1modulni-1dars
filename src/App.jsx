b6f929a931cd7cb819c9f76454138522031c8ce9
Zafar <ergashevzafar1777@gmail.com>
2025-10-06 07:41:20 +0500

    react-1-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <h1>Salom</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>  
+     <h1>Welcome to React JS</h1>
+     <br />
+     <h2>This is my first React app</h2>
+     <br />
+     <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+     </ul>
+     <br />
+     <h3>what is React</h3>
+     <br />
+     <p>React nima? React - bu Facebook tomonidan yaratilgan ochiq kodli JavaScript kutubxonasi bolib,
+      asosan foydalanuvchi interfeyslarini (UI) yaratish uchun moljallangan. U yagona sahifali
+      ilovalar (single-page applications) va dinamik, sezgir veb-ilovalarni ishlab chiqishda keng qollaniladi.
+     </p>
+     <h4>What is Single-page application (SPA)</h4>
+     <p>SPA (Single-Page Application) - bu bitta sahifali veb-ilova bolib,unda sahifa kontenti dinamik
+      rvishda yangilanadi va foydalanuvchining har bir amalida butun sahifani qaytadan yuklash talab qilinmaydi.
+      JavaScript yordamida faqat kerakli kontent ozgartiriladi, bu esa ilovani tezkor va foydalanuvchi uchun qulay qiladi.   </p>
+    </div>
   )
 }
 
